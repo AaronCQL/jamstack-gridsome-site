18fa9396a68d0638d79fb2d3094da3cbb2bd7a0d
Aaron Choo <choozy.aaron@gmail.com>
2020-02-21 21:23:04 +0800

    Add resume icon and transitions

--- src/components/landing/AboutSection.vue
+++ src/components/landing/AboutSection.vue
@@ -7,7 +7,7 @@
     </h1>
 
     <div
-      class="md:shadow-lg md:hover:shadow-xl font-title p-5 max-w-lg rounded-lg mx-auto md:bg-white"
+      class="transition-shadow duration-150 ease-in-out md:shadow-md md:hover:shadow-lg font-title p-5 max-w-lg rounded-lg mx-auto md:bg-white"
     >
       <div class="md:flex">
         <g-image
@@ -15,8 +15,8 @@
           class="md:align-right h-40 w-40 rounded-full mx-auto md:mx-0"
         />
         <div class="md:ml-6 text-center md:text-left">
-          <p class="text-3xl font-semibold">Aaron Choo</p>
-          <div class="text-xl text-gray-700 font-light">
+          <p class="text-4xl md:text-3xl font-semibold">Aaron Choo</p>
+          <div class="text-2xl md:text-xl text-gray-700 font-light">
             <p>Computer Science Student</p>
             <p>Software Engineer Intern</p>
             <p>Ex-Teaching Assistant</p>
@@ -25,9 +25,25 @@
         </div>
       </div>
     </div>
-    <div class="text-center md:mt-24 text-3xl">
-      <font-awesome :icon="['fab', 'github']" class="mx-2" />
-      <font-awesome :icon="['fab', 'linkedin']" class="mx-2" />
+    <div class="text-center md:mt-24 text-4xl text-secondary">
+      <a
+        class="transition duration-150 ease-in-out mx-4 hover:text-primary"
+        href="https://github.com/AaronCQL"
+      >
+        <font-awesome :icon="['fab', 'github']" />
+      </a>
+      <a
+        class="transition duration-150 ease-in-out mx-4 hover:text-primary"
+        href="https://www.linkedin.com/in/aaroncql/"
+      >
+        <font-awesome :icon="['fab', 'linkedin']" />
+      </a>
+      <a
+        class="transition duration-150 ease-in-out mx-4 hover:text-primary"
+        href="#"
+      >
+        <font-awesome :icon="['fas', 'file-alt']" />
+      </a>
     </div>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -11,10 +11,11 @@ import DefaultLayout from "~/layouts/Default.vue";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { config, library } from "@fortawesome/fontawesome-svg-core";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faFileAlt } from "@fortawesome/free-solid-svg-icons";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 
 config.autoAddCss = false;
-library.add(faGithub, faLinkedin);
+library.add(faGithub, faLinkedin, faFileAlt);
 
 export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
