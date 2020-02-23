ba2394b4a8d2e7ee7649073827a8d9f159c7758d
Aaron Choo <choozy.aaron@gmail.com>
2020-02-23 18:57:43 +0800

    Add whitelist

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,55 +1,21 @@
-/* eslint-disable no-undef */
 // This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
-class TailwindExtractor {
-  static extract(content) {
-    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
-  }
-}
-
+// eslint-disable-next-line no-undef
 module.exports = {
   siteName: "Gridsome",
   plugins: [
     {
       use: "gridsome-plugin-tailwindcss",
+      options: {
+        purgeConfig: {
+          whitelist: ["svg-inline--fa"],
+          whitelistPatterns: [/shiki/, /fa-$/],
+        },
+      },
     },
   ],
-  chainWebpack: config => {
-    config.module
-      .rule("css")
-      .oneOf("normal")
-      .use("postcss-loader")
-      .tap(options => {
-        options.plugins.unshift(
-          ...[
-            require("postcss-import"),
-            require("postcss-nested"),
-            require("tailwindcss"),
-          ]
-        );
-
-        if (process.env.NODE_ENV === "production") {
-          options.plugins.push(
-            ...[
-              require("@fullhuman/postcss-purgecss")({
-                content: ["src/assets/**/*.css", "src/**/*.vue", "src/**/*.js"],
-                extractors: [
-                  {
-                    extractor: new TailwindExtractor(),
-                    extensions: ["css", "vue", "js"],
-                  },
-                ],
-                whitelist: ["svg-inline--fa"],
-                whitelistPatterns: [/shiki/, /fa-$/],
-              }),
-            ]
-          );
-        }
-        return options;
-      });
-  },
 };
