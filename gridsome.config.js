dd37bc30b3c1aa13b3c142b461724592e72e487a
Aaron Choo <choozy.aaron@gmail.com>
2020-02-23 18:36:42 +0800

    Whitelist FontAwesome in purgeCSS

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,18 +1,55 @@
+/* eslint-disable no-undef */
 // This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
-// eslint-disable-next-line no-undef
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+  }
+}
+
 module.exports = {
   siteName: "Gridsome",
   plugins: [
     {
       use: "gridsome-plugin-tailwindcss",
-      options: {
-        shouldPurge: false,
-      },
     },
   ],
+  chainWebpack: config => {
+    config.module
+      .rule("css")
+      .oneOf("normal")
+      .use("postcss-loader")
+      .tap(options => {
+        options.plugins.unshift(
+          ...[
+            require("postcss-import"),
+            require("postcss-nested"),
+            require("tailwindcss"),
+          ]
+        );
+
+        if (process.env.NODE_ENV === "production") {
+          options.plugins.push(
+            ...[
+              require("@fullhuman/postcss-purgecss")({
+                content: ["src/assets/**/*.css", "src/**/*.vue", "src/**/*.js"],
+                extractors: [
+                  {
+                    extractor: TailwindExtractor,
+                    extensions: ["css", "vue", "js"],
+                  },
+                ],
+                whitelist: ["svg-inline--fa"],
+                whitelistPatterns: [/shiki/, /fa-$/],
+              }),
+            ]
+          );
+        }
+        return options;
+      });
+  },
 };
