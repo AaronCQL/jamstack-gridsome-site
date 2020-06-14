12525940c65bb22ee908b386eff892eee6ad00aa
Aaron Choo <choozy.aaron@gmail.com>
2020-06-14 10:05:51 +0800

    Rename files

--- src/components/landing/ExperienceSection.vue
+++ src/components/landing/ExperienceSection.vue
@@ -9,19 +9,19 @@
       Refer to my resume for more information about what I had worked on.
     </div>
 
-    <experience-entry
+    <experience-section-entry
       company="Visa Inc."
       date="May 2020 - Present"
       title="Systems Engineer Intern"
     >
-    </experience-entry>
+    </experience-section-entry>
 
     <div
       :class="isDarkMode ? 'border-gray-800' : ''"
       class="border mx-24 my-4"
     ></div>
 
-    <experience-entry
+    <experience-section-entry
       company="Qashier"
       date="Jun 2019 - May 2020"
       title="Software Engineer Intern"
@@ -30,32 +30,32 @@
       <styled-link href="https://hq.qashier.co/#/">web application</styled-link>
       built using Vue.js and Quasar for the frontend, and Firebase, GCP and
       Node.js for the backend.
-    </experience-entry>
+    </experience-section-entry>
 
     <div
       :class="isDarkMode ? 'border-gray-800' : ''"
       class="border mx-24 my-2"
     ></div>
 
-    <experience-entry
+    <experience-section-entry
       company="National University of Singapore"
       date="Aug 2019 - Dec 2019"
       title="Teaching Assistant"
     >
       Taught weekly tutorial sessions for CS1101S Programming Methodology I
-    </experience-entry>
+    </experience-section-entry>
   </div>
 </template>
 
 <script>
-import ExperienceEntry from "./ExperienceEntry.vue";
+import ExperienceSectionEntry from "./ExperienceSectionEntry.vue";
 import StyledLink from "./../shared/StyledLink";
 import SectionHeader from "./../shared/SectionHeader";
 
 export default {
   name: "ExperienceSection",
   components: {
-    ExperienceEntry,
+    ExperienceSectionEntry,
     StyledLink,
     SectionHeader,
   },
--- src/components/landing/ProjectSection.vue
+++ src/components/landing/ProjectSection.vue
@@ -4,55 +4,58 @@
       Personal Projects
     </section-header>
 
-    <project-entry title="Tree Secure (Mobile App)">
+    <project-section-entry title="Tree Secure (Mobile App)">
       <p>
         A not-so-serious cross-platform mobile app to spread the awareness of
         the heritage trees in Singapore. Made with Flutter and developed in 24
         hours at NUS Hack&Roll 2020.
       </p>
-    </project-entry>
+    </project-section-entry>
 
-    <project-entry class="mt-4" title="PixiesPixel (Game)">
+    <project-section-entry class="mt-4" title="PixiesPixel (Game)">
       <p>
         A cross-platform realtime online multiplayer free-for-all 2D platformer
         built using Godot and GDScript. Achieved Honourable Mention (top 4/243
         teams) at NUS Orbital 2019.
       </p>
-    </project-entry>
+    </project-section-entry>
 
-    <project-entry class="mt-4" title="Computer Organisation Tool (Website)">
+    <project-section-entry
+      class="mt-4"
+      title="Computer Organisation Tool (Website)"
+    >
       <p>
         A learning tool for CS2100 made for the web browser. Tools include
         number base converter, IEEE 754 single-precision format converter, truth
         table generator, MIPS Architecture instruction encoder.
       </p>
-    </project-entry>
+    </project-section-entry>
 
-    <project-entry class="mt-4" title="Telegram Weather Bot">
+    <project-section-entry class="mt-4" title="Telegram Weather Bot">
       <p>
         A simple telegram bot that tells the current and forecast weather at 16
         specific locations in Singapore. Built using Python in a day-long
         hackathon at HackNTU 2019.
       </p>
-    </project-entry>
+    </project-section-entry>
 
-    <project-entry class="mt-4" title="Telegram Bot Tutorial">
+    <project-section-entry class="mt-4" title="Telegram Bot Tutorial">
       <p>
         A step-by-step guide on building a free serverless Telegram bot with
         Firebase Functions.
       </p>
-    </project-entry>
+    </project-section-entry>
   </div>
 </template>
 
 <script>
-import ProjectEntry from "~/components/landing/ProjectEntry.vue";
+import ProjectSectionEntry from "~/components/landing/ProjectSectionEntry.vue";
 import SectionHeader from "./../shared/SectionHeader";
 
 export default {
   name: "ProjectSection",
   components: {
-    ProjectEntry,
+    ProjectSectionEntry,
     SectionHeader,
   },
 };
